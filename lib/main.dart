749a8d84386f6a29009393349dfa831df2c0d1c0
Maksim Lin <maks@manichord.com>
2021-03-30 20:04:45 +1100

    min working: using CC to turn on-off all lights

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,19 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_midi_command/flutter_midi_command.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String? title;
 
   @override
@@ -47,67 +32,105 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  StreamSubscription<String>? _setupSubscription;
+  StreamSubscription<MidiPacket>? _dataSubscription;
+  MidiCommand _midiCommand = MidiCommand();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  MidiDevice? connectedDevice;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listDevices();
+
+    _setupSubscription = _midiCommand.onMidiSetupChanged?.listen((data) {
+      print("setup changed $data");
+
+      switch (data) {
+        case "deviceFound":
+          print('found: $data');
+          setState(() {});
+          break;
+        // case "deviceOpened":
+        //   break;
+        default:
+          print("Unhandled setup change: $data");
+          break;
+      }
     });
+
+    _dataSubscription = _midiCommand.onMidiDataReceived?.listen((packet) {
+      print("MIDI data: $packet");
+      for (var d in packet.data) {
+        print('d:$d');
+      }
+    });
+  }
+
+  void _listDevices() async {
+    final devices = await _midiCommand.devices;
+
+    if (devices != null) {
+      for (var d in devices) {
+        print('MIDI DEVICE: [${d.id}] "${d.name}" ${d.inputPorts}');
+
+        if (d.name == 'FL STUDIO FIRE') {
+          print('connect to device: ${d.id} ${d.name}');
+          _midiCommand.connectToDevice(d);
+          connectedDevice = d;
+        }
+      }
+    } else {
+      print('NULL devices');
+    }
+  }
+
+  void sendAllOff() {
+    print('sending all OFF to:');
+    _midiCommand.sendData(Uint8List.fromList([0xB0, 0x7F, 0]));
+  }
+
+  void sendAllOn() {
+    print('sending all ON to:');
+    _midiCommand.sendData(Uint8List.fromList([0xB0, 0x7F, 1]));
+  }
+
+  void disconnect() {
+    final d = connectedDevice;
+    if (d != null) {
+      _midiCommand.disconnectDevice(d);
+      print('DISCONNECTED ${d.id} ${d.name}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title!),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            TextButton(
+              onPressed: sendAllOff,
+              child: Text('ALL OFF'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextButton(
+              onPressed: sendAllOn,
+              child: Text('ALL ON'),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          disconnect();
+        },
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
